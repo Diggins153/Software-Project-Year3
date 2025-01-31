50844c802e7103376f6f08076e34a239b818de08
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-01-31 17:29:27 +0000

    Add register form schema

---
+++ src/lib/formSchemas.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const registerFormSchema = z.object({
+    displayName: z
+        .string()
+        .min(0, "Please enter a display name.")
+        .max(40, "Your display name cannot be longer than 40 character."),
+    email: z
+        .string()
+        .email("Please enter an email address.")
+        .max(64, "The email address cannot be longer than 64 characters. Please choose a different one."),
+    password: z
+        .string()
+        .min(10, "Password must be at least 10 characters long."),
+});

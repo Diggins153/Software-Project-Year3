84d8fb61b9ca0e4f8d1ca5754ac33f80e39beed6
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-01-31 16:24:12 +0000

    Add MikroORM config

---
+++ src/config/mikro-orm.ts
@@ -0,0 +1,26 @@
+import { Class } from "@/entities/Class";
+import { Payment } from "@/entities/Payment";
+import { Race } from "@/entities/Race";
+import { User } from "@/entities/User";
+import { VerificationCode } from "@/entities/VerificationCode";
+import { Options } from "@mikro-orm/core";
+import { MySqlDriver } from "@mikro-orm/mysql";
+
+const config: Options = {
+    dbName: process.env.MYSQL_DB,
+    driver: MySqlDriver,
+    entities: [
+        User,
+        Class,
+        Payment,
+        Race,
+        VerificationCode,
+    ],
+    debug: process.env.DEBUG === "true" || process.env.DEBUG?.includes("db"),
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: parseInt(process.env.MYSQL_PORT!),
+};
+
+export default config;

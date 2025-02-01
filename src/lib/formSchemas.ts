45c4c245ba252b55aebb341cd3d0501f7679e421
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-01 12:09:03 +0000

    Add form validations to register form schema

--- src/lib/formSchemas.ts
+++ src/lib/formSchemas.ts
@@ -1,3 +1,5 @@
+import { hasDigits, hasLowercase, hasSpecialCharacter, hasUppercase } from "@/lib/formValidation";
+import { userExists } from "@/lib/formValidationServer";
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
@@ -15,10 +17,28 @@ export const registerFormSchema = z.object({
     email: z
         .string()
         .email("Please enter an email address.")
-        .max(64, "The email address cannot be longer than 64 characters. Please choose a different one."),
+        .max(64, "The email address cannot be longer than 64 characters. Please choose a different one.")
+        .refine(async (value) => !await userExists(value), "This email address is already in use. Please choose a different one"),
     password: z
         .string()
-        .min(10, "Password must be at least 10 characters long."),
+        .min(10, "Password must be at least 10 characters long.")
+        .superRefine((value, ctx) => {
+            if (!hasSpecialCharacter(value)) {
+                ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Password must have a special character." });
+            }
+
+            if (!hasDigits(value)) {
+                ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Password must have a digit." });
+            }
+
+            if (!hasUppercase(value)) {
+                ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Password must have an uppercase letter." });
+            }
+
+            if (!hasLowercase(value)) {
+                ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Password must have a lowercase letter." });
+            }
+        }),
     gdpr: z
         .boolean({ required_error: "You need to accept the terms to continue." })
         .default(false)

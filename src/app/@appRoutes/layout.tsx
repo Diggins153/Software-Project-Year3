a313fe1f22321c820d0166e9a9e0e878b85e15bd
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-05 22:23:59 +0000

    Move app group layout to appRoutes parallel route and remove logged in check

--- src/app/@appRoutes/layout.tsx
+++ src/app/@appRoutes/layout.tsx
@@ -1,17 +1,9 @@
-import { auth, signIn } from "@/auth";
 import Sidebar from "@/components/Sidebar";
 import { ReactNode } from "react";
 
 export default async function AppLayout({ children }: Readonly<{ children: ReactNode; }>) {
-    const session = await auth();
-    if (!session?.user) {
-        await signIn();
-    }
-
     return <div className="flex">
-        {/* Sidebar on the left */ }
         <Sidebar/>
-        {/* Main content */ }
         <div className="content">
             { children }
         </div>

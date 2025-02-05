311fe0fd118a1c96164cf0e3114ce8465fbc1d47
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-05 21:37:12 +0000

    Edit sidebar - be a server component by default - use nav item components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,48 +1,62 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
+import { auth, signOut } from "@/auth";
+import NavItem from "@/components/NavItem";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { alikeAngular } from "@/lib/fonts";
+import { CogIcon, DoorOpenIcon, KeyIcon, LogOutIcon, SwordsIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 
-export default function Sidebar() {
-    const [ showDropdown, setShowDropdown ] = useState(false);
+export default async function Sidebar() {
+    const session = await auth();
 
     return (
-        <div className="sidebar">
-            <div className="sidebar-header">BeyonD&D</div>
-            <nav>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/campaigns">Campaigns</Link></li>
-
-                    {/* Characters Dropdown */ }
-                    <li
-                        className="dropdown"
-                        onMouseEnter={ () => setShowDropdown(true) }
-                        onMouseLeave={ () => setShowDropdown(false) }
+        <div className="h-dvh flex flex-col gap-4 p-2.5">
+            <nav className="flex flex-col gap-5 h-full justify-center">
+                <div className="flex flex-col gap-1">
+                    <TooltipProvider delayDuration={ 0 }>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="/"
+                                    className={ `${ alikeAngular.className } text-4xl font-bold text-white bg-[#330F0A] rounded-lg text-center px-5 py-3 select-none` }
+                                >
+                                    B
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent
+                                side="right"
+                                className={ `text-2xl text-white bg-[#330F0A] font-bold ${ alikeAngular.className }` }
+                            >
+                                <p>ByonD&D - Home</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </div>
+                <div className="flex flex-col gap-1">
+                    { !!session?.user
+                        ? <>
+                            {/*<NavItem title="Home" href="/" icon={ <HomeIcon size={ 24 }/> }/>*/ }
+                            <NavItem title="Campaigns" href="/campaigns" icon={ <SwordsIcon size={ 24 }/> }/>
+                            <NavItem title="Characters" href="/characters" icon={ <UsersIcon size={ 24 }/> }/>
+                            {/*<NavItem title="Dashboard" href="/dashboard" icon={ <LayoutDashboardIcon size={ 24 }/> }/>*/ }
+                        </>
+                        : <>
+                            <NavItem title="Login" href="/login" icon={ <KeyIcon size={ 24 }/> }/>
+                            <NavItem title="Register" href="/register" icon={ <DoorOpenIcon size={ 24 }/> }/>
+                        </>
+                    }
+                </div>
+                <div className="flex flex-col gap-1">
+                    <NavItem title="Settings" href="/settings" icon={ <CogIcon size={ 24 }/> }/>
+                    <form
+                        action={ async () => {
+                            "use server";
+                            await signOut();
+                        } }
                     >
-                        <Link href="/character">Characters</Link>
-                        { showDropdown && (
-                            <ul className="dropdown-menu">
-                                <li><Link href="#">Character 1</Link></li>
-                                <li><Link href="#">Character 2</Link></li>
-                                <li><Link href="#">Character 3</Link></li>
-                            </ul>
-                        ) }
-                    </li>
-
-                    <li><Link href="/contact">Contact</Link></li>
-                    <li><Link href="/dashboard">Dashboard</Link></li>
-                    <li><Link href="/login">Login</Link></li>
-                    <li><Link href="/register">Register</Link></li>
-                </ul>
+                        <NavItem title="Sign Out" icon={ <LogOutIcon size={ 24 }/> }/>
+                    </form>
+                </div>
             </nav>
-
-            <div className="bottom-link">
-                <Link href="/settings">Settings</Link>
-                <Button type="button" variant="ghost" onClick={ () => signOut() }>Log Out</Button>
-            </div>
         </div>
     );
 }

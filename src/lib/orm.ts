2be87dc889223b017395ba665fe9228dc6bda067
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-24 16:04:36 +0000

    Type orm cache variable

--- src/lib/orm.ts
+++ src/lib/orm.ts
@@ -1,10 +1,10 @@
 import config from "@/config/mikro-orm";
 import { MikroORM } from "@mikro-orm/core";
 
-let orm: any = null;
+let orm: MikroORM | null = null;
 
 export default async function getORM() {
     if (orm == null) orm = await MikroORM.init(config);
 
-    return orm.fork();
+    return orm.em.fork();
 }

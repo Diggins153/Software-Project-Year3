7ba827955ffc1c069319a8f9498b5ae6eb40049d
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-24 14:39:22 +0000

    Add ts files to webpack config

--- next.config.js
+++ next.config.js
@@ -4,21 +4,19 @@ const { devDependencies } = require("./package.json");
 // Build up your externals object from devDependencies (only for the server build).
 const externals = {};
 for (const devDependency of Object.keys(devDependencies)) {
-    externals[devDependency] = `commonjs ${devDependency}`;
+    externals[devDependency] = `commonjs ${ devDependency }`;
 }
 
 // Optional: Add any optional modules that you want to be externalized
 const optionalModules = new Set([
     ...Object.keys(require("knex/package.json").browser),
-    ...Object.keys(
-        require("@mikro-orm/core/package.json").peerDependencies || {}
-    ),
+    ...Object.keys(require("@mikro-orm/core/package.json").peerDependencies || {}),
     ...Object.keys(require("@mikro-orm/core/package.json").devDependencies || {}),
 ]);
 
 // Externalize all MikroORM modules
 externals["@mikro-orm/core"] = "commonjs @mikro-orm/core";
-externals["@mikro-orm/mariadb"] = "commonjs @mikro-orm/mariadb";
+externals["@mikro-orm/mysql"] = "commonjs @mikro-orm/mysql";
 externals["@mikro-orm/migrations"] = "commonjs @mikro-orm/migrations";
 externals["@mikro-orm/reflection"] = "commonjs @mikro-orm/reflection";
 externals["@mikro-orm/seeder"] = "commonjs @mikro-orm/seeder";
@@ -30,9 +28,7 @@ externals["knex"] = "commonjs knex";
 module.exports = {
     output: "standalone",
     experimental: {
-        turbo: {
-
-        }
+        turbo: {},
     },
     webpack: (config, { isServer, webpack }) => {
         //
@@ -57,6 +53,11 @@ module.exports = {
         //
         config.module.rules.push(
             {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+            },
+            {
                 test: /\.node$/,
                 use: "node-loader",
             },
@@ -64,7 +65,7 @@ module.exports = {
                 test: /\.mjs$/,
                 include: /node_modules/,
                 type: "javascript/auto",
-            }
+            },
         );
 
         // Return the modified config to Next

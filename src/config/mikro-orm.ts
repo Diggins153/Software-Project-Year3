85e09340b8247246fd4c9b297d5877f37079c948
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-25 16:43:48 +0000

    Add warnings to mikro-orm config

--- src/config/mikro-orm.ts
+++ src/config/mikro-orm.ts
@@ -6,15 +6,16 @@ import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 const config: Options = {
     driver: MySqlDriver,
     metadataProvider: TsMorphMetadataProvider,
-    // Entities must be specified explicitly
     entities: [ User ],
     entitiesTs: [ User ],
+    // WARNING: Entities must be specified explicitly
     dbName: process.env.MYSQL_DB,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     port: parseInt(process.env.MYSQL_PORT!),
     debug: process.env.DEBUG === "true",
+    // WARNING: Keep the discovery option as is
     discovery: { disableDynamicFileAccess: true },
 };
 

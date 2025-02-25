9817db44ec65287f4596642ae048d475874eefcd
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-25 18:24:00 +0000

    Remove session checking as at this stage the user is already authenticated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,12 @@
-import { auth, signOut } from "@/auth";
+import { signOut } from "@/auth";
 import NavItem from "@/components/NavItem";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { alikeAngular } from "@/lib/fonts";
 import { CogIcon, DoorOpenIcon, KeyIcon, LogOutIcon, SwordsIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Sidebar() {
-    const session = await auth();
-
     return (
         <div className="h-dvh flex flex-col gap-4 p-2.5">
             <nav className="flex flex-col gap-5 h-full justify-center">
@@ -32,18 +31,8 @@ export default async function Sidebar() {
                     </TooltipProvider>
                 </div>
                 <div className="flex flex-col gap-1">
-                    { !!session?.user
-                        ? <>
-                            {/*<NavItem title="Home" href="/" icon={ <HomeIcon size={ 24 }/> }/>*/ }
-                            <NavItem title="Campaigns" href="/campaigns" icon={ <SwordsIcon size={ 24 }/> }/>
-                            <NavItem title="Characters" href="/characters" icon={ <UsersIcon size={ 24 }/> }/>
-                            {/*<NavItem title="Dashboard" href="/dashboard" icon={ <LayoutDashboardIcon size={ 24 }/> }/>*/ }
-                        </>
-                        : <>
-                            <NavItem title="Login" href="/login" icon={ <KeyIcon size={ 24 }/> }/>
-                            <NavItem title="Register" href="/register" icon={ <DoorOpenIcon size={ 24 }/> }/>
-                        </>
-                    }
+                    <NavItem title="Campaigns" href="/campaigns" icon={ <SwordsIcon size={ 24 }/> }/>
+                    <NavItem title="Characters" href="/characters" icon={ <UsersIcon size={ 24 }/> }/>
                 </div>
                 <div className="flex flex-col gap-1">
                     <NavItem title="Settings" href="/settings" icon={ <CogIcon size={ 24 }/> }/>

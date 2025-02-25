1cb5fe96f93a4a88f002a9918f2d5bdb291ebd8f
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-25 15:16:53 +0000

    Specify entities explicitly

--- src/config/mikro-orm.ts
+++ src/config/mikro-orm.ts
@@ -1,3 +1,4 @@
+import { User } from "@/entities/User";
 import { Options } from "@mikro-orm/core";
 import { MySqlDriver } from "@mikro-orm/mysql";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
@@ -5,15 +6,16 @@ import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 const config: Options = {
     driver: MySqlDriver,
     metadataProvider: TsMorphMetadataProvider,
-    entities: [ "src/entities/*.ts", ],
-    entitiesTs: [ "src/entities/*.ts", ],
+    // Entities must be specified explicitly
+    entities: [ User ],
+    entitiesTs: [ User ],
     dbName: process.env.MYSQL_DB,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     port: parseInt(process.env.MYSQL_PORT!),
     debug: process.env.DEBUG === "true",
-    discovery: { disableDynamicFileAccess: false },
+    discovery: { disableDynamicFileAccess: true },
 };
 
 export default config;

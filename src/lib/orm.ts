f25ea76e78924fb8d1da3f22f11b697a87a682af
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-27 12:54:06 +0000

    Remove running migrations on ORM init

--- src/lib/orm.ts
+++ src/lib/orm.ts
@@ -6,8 +6,6 @@ let orm: MikroORM | null = null;
 export default async function getORM() {
     if (orm == null) {
         orm = await MikroORM.init(config);
-
-        await orm.migrator.up();
     }
 
     return orm.em.fork();

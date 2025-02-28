3bbc9fced93c54f706c08d8fffe4b33610ff4c16
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-28 21:56:53 +0000

    Add Create Premade Character server action

---
+++ src/lib/actions/characters.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { Character } from "@/entities/Character";
+import { CharacterClasses } from "@/entities/CharacterClasses";
+import { Class } from "@/entities/Class";
+import { Race } from "@/entities/Race";
+import { User } from "@/entities/User";
+import { auth } from "@/lib/auth";
+import getORM from "@/lib/orm";
+import { ref } from "@mikro-orm/core";
+import { redirect } from "next/navigation";
+
+export async function createCharacter(/* Put character deatails as arguments here */) {
+
+}
+
+export async function createPremadeCharacter(name: string, race: string, charClass: string) {
+    const session = await auth();
+
+    if (!session || !session.user) {
+        return redirect("/");
+    }
+
+    const em = await getORM();
+    const user = await em.findOne(User, { email: session.user.email });
+
+    if (user == null) {
+        return { ok: false };
+    }
+
+    const dbRace = await em.findOne(Race, { name: race });
+    const dbClass = await em.findOne(Class, { name: charClass });
+    const newChar = new Character(name, `handle:${ name }`, dbRace!);
+
+    newChar.image = "https://placehold.co/75.png";
+    newChar.classes.add(new CharacterClasses(ref(newChar), dbClass!, 1));
+    user.characters.add(newChar);
+
+    await em.flush();
+
+    return redirect("/characters");
+}

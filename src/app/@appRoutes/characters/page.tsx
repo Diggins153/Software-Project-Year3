e7460a2b5fc469a1e5a7be604759d2ca8a96e110
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-28 12:49:36 +0000

    Add testing characters to character page

--- src/app/@appRoutes/characters/page.tsx
+++ src/app/@appRoutes/characters/page.tsx
@@ -1,5 +1,13 @@
+import CharacterCard from "@/components/characters/CharacterCard";
+
 export default function Characters() {
+    const characters = [
+        { id: 1, name: "Arskit", image: "https://placehold.co/75.jpg" },
+        { id: 2, name: "Niklas Eeroson", image: "https://placehold.co/75.jpg" },
+    ];
+
     return <main>
         <h1 className="text-2xl">Characters Page</h1>
+        { characters.map(character => <CharacterCard key={ character.id } { ...character }/>) }
     </main>;
 }

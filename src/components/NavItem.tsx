885766dd48f362a85c51090e220469e532cc9870
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-01 18:44:30 +0000

    Add active route nav link highlight

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { artifika } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { PropsWithoutRef } from "react";
 
 export default function NavItem({ title, href, icon }: PropsWithoutRef<{ title: string, href?: string, icon?: any }>) {
+    const pathname = usePathname();
 
     return <>
-        <TooltipProvider delayDuration={ 0 } disableHoverableContent={true}>
+        <TooltipProvider delayDuration={ 0 } disableHoverableContent={ true }>
             <Tooltip>
                 <TooltipTrigger asChild>
                     { !!href
                         ? <Link
                             href={ href }
-                            className="!flex bg-green-800 text-white px-5 py-3 justify-center rounded-lg w-full"
+                            className={ cn(
+                                "!flex bg-green-800 text-white px-5 py-3 justify-center rounded-lg w-full",
+                                { "bg-theme": pathname.startsWith(href) },
+                            ) }
                         >
                             { !!icon
                                 ? <div>{ icon }</div>
@@ -30,7 +35,7 @@ export default function NavItem({ title, href, icon }: PropsWithoutRef<{ title: 
                         </button>
                     }
                 </TooltipTrigger>
-                <TooltipContent side={ "right" } className={ `bg-green-800 text-white text-md select-none ${artifika.className}` }>
+                <TooltipContent side="right" className="bg-green-800 text-white text-md select-none">
                     <p>{ title }</p>
                 </TooltipContent>
             </Tooltip>

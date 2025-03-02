ba460672dddc7efdedb8b808efc5b042d1e31048
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-02 15:32:58 +0000

    Edit campaign and campaign card to use ORM

--- src/app/@appRoutes/campaigns/page.tsx
+++ src/app/@appRoutes/campaigns/page.tsx
@@ -1,15 +1,16 @@
 import CampaignCard from "@/components/CampaignCard";
+import { Campaign } from "@/entities/Campaign";
+import getORM from "@/lib/orm";
 import React from "react";
 
-// Note: Tempfix before Campaign entity is implemented
-// TODO: Use ORM
 export default async function CampaignsPage() {
-    const cardData: any[] = [];
+    const campaignsRepo = (await getORM()).getRepository(Campaign);
+    const cardData = await campaignsRepo.findAll({ populate: [ "dungeonMaster" ] });
 
     return (
         <main>
             <div>
-                { cardData.map(campaign => <CampaignCard { ...campaign } key={ campaign.id }/>) }
+                { cardData.map(campaign => <CampaignCard campaign={ campaign } key={ campaign.id }/>) }
             </div>
         </main>
     );

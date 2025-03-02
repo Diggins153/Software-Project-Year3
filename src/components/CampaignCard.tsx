e6efdaf7d4b0e2c3cc57268b91bc1dc36481641e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-02 16:03:48 +0000

    Edit campaign and campaign card to use ORM

--- src/components/CampaignCard.tsx
+++ src/components/CampaignCard.tsx
@@ -1,9 +1,7 @@
 import { Campaign } from "@/entities/Campaign";
-import { Loaded } from "@mikro-orm/core";
-import React from "react";
 import styles from "./CampaignCard.module.css";
 
-export default async function CampaignCard({ campaign }: { campaign: Loaded<Campaign> }) {
+export default async function CampaignCard({ campaign }: { campaign: Campaign }) {
     const { id, name, createdAt, dungeonMaster, maxPlayers, outline } = campaign;
 
     return (
@@ -12,7 +10,6 @@ export default async function CampaignCard({ campaign }: { campaign: Loaded<Camp
             <img className={ styles.cardImage } src="https://placehold.co/400" alt="Placeholder"/>
             <p><strong>ID:</strong> { id }</p>
             <p><strong>Created At:</strong> { new Date(createdAt).toLocaleDateString() }</p>
-            {/*@ts-ignore*/}
             <p><strong>Dungeon Master:</strong> { dungeonMaster.displayName }</p>
             <p><strong>Max Players:</strong> { maxPlayers }</p>
             <p><strong>Outline:</strong> { outline }</p>

10331521128fa1860d12efe6e3fc169515ab58e6
AlexC <alexclinton133@gmail.com>
2025-03-03 19:49:40 +0000

    Added new your_characters page to view users characters

---
+++ src/app/@appRoutes/characters/your_characters/page.tsx
@@ -0,0 +1,43 @@
+import {auth} from "@/lib/auth";
+import {redirect} from "next/navigation";
+import getORM from "@/lib/orm";
+import {Character} from "@/entities/Character";
+import CharacterCard from "@/components/characters/CharacterCard";
+
+export default async function CharactersPage() {
+    const session = await auth();
+
+    if (!session || !session.user) {
+        redirect("/");
+    }
+
+    const em = await getORM();
+    const characters = await em.find(
+        Character,
+        { owner: { email: session.user.email } },
+        { disableIdentityMap: true, populate: [ "*" ] },
+    );
+
+    return (
+        <main>
+            <div className="space-y-4">
+                <h1 className="text-3xl font-bold text-center">Your Characters</h1>
+                <div className="grid grid-cols-2 gap-2.5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+                    <CharacterCard
+                        character={ {
+                            id: "create",
+                            name: "Create a new Character",
+                            image: "/icons/plus.png",
+                        } as any as Character }
+                    />
+                    { characters.length > 0
+                        ? characters.map(character => <CharacterCard
+                            key={ character.id }
+                            character={ character }
+                        />)
+                        : <CharacterCard/> }
+                </div>
+            </div>
+        </main>
+    );
+}

7052d43af432393147a1baf919285a2b99076226
AlexC <alexclinton133@gmail.com>
2025-03-03 19:48:15 +0000

    Added new page to display currently logged in users characters

--- src/app/@appRoutes/characters/page.tsx
+++ src/app/@appRoutes/characters/page.tsx
@@ -12,33 +12,8 @@ export default async function CharactersPage() {
         redirect("/");
     }
 
-    const em = await getORM();
-    const characters = await em.find(
-        Character,
-        { owner: { email: session.user.email } },
-        { disableIdentityMap: true, populate: [ "*" ] },
-    );
-
     return (
         <main>
-            <div className="space-y-4">
-                <h1 className="text-3xl font-bold text-center">Your Characters</h1>
-                <div className="grid grid-cols-2 gap-2.5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-                    <CharacterCard
-                        character={ {
-                            id: "create",
-                            name: "Create a new Character",
-                            image: "/icons/plus.png",
-                        } as any as Character }
-                    />
-                    { characters.length > 0
-                        ? characters.map(character => <CharacterCard
-                            key={ character.id }
-                            character={ character }
-                        />)
-                        : <CharacterCard/> }
-                </div>
-            </div>
 
             <div className="p-6 flex flex-col items-center gap-6">
                 <h1 className="text-3xl font-bold text-center">Create Your Character</h1>
@@ -67,6 +42,19 @@ export default async function CharactersPage() {
                                 <p className="text-gray-600 text-center">
                                     Design your own character from scratch with complete creative freedom.
                                     Choose your race, class, abilities, and more!
+                                </p>
+                            </div>
+                        </div>
+                    </Link>
+
+                    <Link href="/characters/your_characters">
+                        <div className="w-[300px] h-[750px] p-6 bg-yellow-200 rounded-lg shadow-lg border border-gray-200 transition-all duration-200 hover:shadow-2xl hover:scale-105 hover:border-gray-400 active:scale-95 cursor-pointer flex flex-col">
+                            <div className="mt-4 text-center">
+                                <h2 className="text-xl font-bold text-gray-800">View Your Characters</h2>
+                            </div>
+                            <div className="flex-grow flex items-center justify-center">
+                                <p className="text-gray-600 text-center">
+                                    See a detailed list of all your created characters and manage them.
                                 </p>
                             </div>
                         </div>

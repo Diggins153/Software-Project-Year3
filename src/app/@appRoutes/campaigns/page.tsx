32f8201a38db165d7672609f593247fb86b7b39e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-04 14:30:47 +0000

    Add create button to campaigns page

--- src/app/@appRoutes/campaigns/page.tsx
+++ src/app/@appRoutes/campaigns/page.tsx
@@ -1,6 +1,8 @@
 import CampaignCard from "@/components/CampaignCard";
+import { buttonVariants } from "@/components/ui/button";
 import { Campaign } from "@/entities/Campaign";
 import getORM from "@/lib/orm";
+import Link from "next/link";
 import React from "react";
 
 export default async function CampaignsPage() {
@@ -9,6 +11,7 @@ export default async function CampaignsPage() {
 
     return (
         <main>
+            <Link className={ buttonVariants() } href="/campaigns/create">Create Campaign</Link>
             <div>
                 { cardData.map(campaign => <CampaignCard campaign={ campaign } key={ campaign.id }/>) }
             </div>

1a5e5abb3c05be4a81ac884976ba2c0589b0ea4f
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-04 19:24:50 +0000

    Make character use SQL

--- src/lib/actions/characters.ts
+++ src/lib/actions/characters.ts
@@ -5,37 +5,26 @@ import { CharacterClass } from "@/entities/CharacterClass";
 import { Class } from "@/entities/Class";
 import { Race } from "@/entities/Race";
 import { User } from "@/entities/User";
+import { fetchUser } from "@/lib/actions/authentication";
 import { auth } from "@/lib/auth";
-import getORM from "@/lib/orm";
+import query from "@/lib/query";
 import { redirect } from "next/navigation";
-import {ref} from "@mikro-orm/core";
 
 export async function createCharacter(name: string, race: string) {
     const session = await auth();
     if (!session || !session.user) return redirect("/");
 
-    const em = await getORM();
-    const user = await em.findOne(User, { email: session.user.email });
+    const user = await fetchUser(session.user.email!);
     if (!user) return { ok: false };
 
     // Retrieve the Race entity based on the provided race name.
-    const dbRace = await em.findOne(Race, { name: race });
+    const dbRace = await query("SELECT name FROM race WHERE name = ?", race) as Race;
     if (!dbRace) return { ok: false, error: "Invalid race" };
 
-    // Create a new character instance.
-    const newChar = new Character(name, `handle:${name}`, dbRace);
-    newChar.image = "https://placehold.co/75.png";
-
-    // Set the owner so that the required owner_id field is populated.
-    newChar.owner = ref(user);
-
     // Add the character to the user's collection.
-    user.characters.add(newChar);
+    query("INSERT INTO `character` (created_at, updated_at, name, handle, race_id, image, owner_id) VALUE (now(), now(), ?, ?, ?, '', ?)", name, `@${ name }`, dbRace.id, session.user.id);
 
     // Persist and flush the new character so it gets an ID.
-    em.persist(newChar);
-    await em.flush();
-
     return redirect("/characters");
 }
 
@@ -46,28 +35,16 @@ export async function createPremadeCharacter(name: string, race: string, charCla
         return redirect("/");
     }
 
-    const em = await getORM();
-    const user = await em.findOne(User, { email: session.user.email });
-
-    if (user == null) {
-        return { ok: false };
-    }
-
-    const dbRace = await em.findOne(Race, { name: race });
+    const dbRace = await query("SELECT * FROM race WHERE name = ?", race) as Race;
     if (dbRace == null) {
         return { ok: false, message: "Invalid race" };
     }
-    const dbClass = await em.findOne(Class, { name: charClass });
+    const dbClass = await query("SELECT * FROM class WHERE name = ?", charClass) as Class;
     if (dbClass == null) {
         return { ok: false, message: "Invalid class" };
     }
-    const newChar = new Character(name, `@${ name }`, dbRace);
 
-    newChar.image = "https://placehold.co/75.png";
-    newChar.classes.add(new CharacterClass(newChar, dbClass, 1));
-    user.characters.add(newChar);
-
-    await em.flush();
+    await query("INSERT INTO `character` (created_at, updated_at, name, handle, race_id, image, owner_id) VALUE (now(), now(), ?, ?, ?, '', ?)", name, `@${ name }`, dbRace.id, session.user.id);
 
     return redirect("/characters");
 }

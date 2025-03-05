9ee71def17c5f2eefb2387bee6a0981df2c1ab75
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-05 12:22:45 +0000

    Add Class Tokens to character card

--- src/components/characters/CharacterCard.tsx
+++ src/components/characters/CharacterCard.tsx
@@ -1,10 +1,15 @@
+import ClassTokens from "@/components/characters/ClassTokens";
+import query from "@/lib/database";
 import { Character } from "@/types/Character";
+import { CharacterClass } from "@/types/CharacterClass";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function CharacterCard({ character }: { character?: Character }) {
     if (!!character) {
         const { id, image = "", name } = character;
+        const classes = await query<CharacterClass[]>("SELECT character_class.*, c.name AS class_name FROM character_class JOIN class c ON c.id = character_class.class_id WHERE character_id = ?", id);
+
         return <Link
             href={ `/characters/${ id }` }
             className="bg-yellow-200 text-black flex flex-col items-center justify-center mt-[calc(75px/2)] rounded-lg border-2 border-transparent"
@@ -12,7 +17,7 @@ export default async function CharacterCard({ character }: { character?: Charact
             <div className="relative top-[calc(-75px/2)] mb-[calc(-75px/2)]">
                 <Image src={ image } alt="" width={ 75 } height={ 75 } className="rounded-full bg-white"/>
                 <div className="absolute left-full top-full -translate-y-full -translate-x-[25px]">
-                    {/*<ClassTokens classes={ character.classes }/>*/}
+                    <ClassTokens classes={ classes }/>
                 </div>
             </div>
             <div className="text-lg p-1 select-none text-center">

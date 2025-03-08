b3d53d52fbfee171c5aa29a6dea6bada9bafa2bb
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-08 19:39:00 +0000

    Create delete character server action

--- src/lib/actions/characters.ts
+++ src/lib/actions/characters.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import query from "@/lib/database";
+import { Character } from "@/types/Character";
 import { Class } from "@/types/Class";
 import { Race } from "@/types/Race";
 import { fetchUser } from "@/lib/actions/authentication";
@@ -44,4 +45,28 @@ export async function createPremadeCharacter(name: string, race: string, charCla
     await query("INSERT INTO `character` (created_at, updated_at, name, handle, race_id, image, owner_id) VALUE (now(), now(), ?, ?, ?, '', ?)", name, `@${ name }`, dbRace.id, session.user.id);
 
     return redirect("/characters");
+}
+
+export async function deleteCharacter(characterId: number): Promise<
+    { ok: false, message: string } |
+    { ok: true, message: string, redirect: string }
+> {
+    const session = await auth();
+    // Session check
+    if (!session || !session.user) return redirect("/characters");
+
+    const character = (await query<Character[]>("SELECT name, owner_id FROM `character` WHERE id = ?", characterId))[0] || null;
+
+    // Check the character exists
+    if (!character) return { ok: false, message: "Could not find that character." };
+
+    // Check the current user owns that character
+    if (character.owner_id.toString() != session.user.id)
+        return { ok: false, message: "Sorry, you are not allowed to delete someone else's character." };
+
+    // Delete character
+    await query("DELETE FROM character_class WHERE character_id = ?", characterId);
+    await query("DELETE FROM `character` WHERE id = ?", characterId);
+
+    return { ok: true, message: `${ character.name } deleted.`, redirect: "/characters" };
 }

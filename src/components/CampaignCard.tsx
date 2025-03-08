48f162a32acdcd55f7a01af41564fec2b12508c1
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-08 16:11:13 +0000

    Update campaign card to use SQL

--- src/components/CampaignCard.tsx
+++ src/components/CampaignCard.tsx
@@ -1,17 +1,17 @@
-import { Campaign } from "@/entities/Campaign";
+import { Campaign } from "@/types/Campaign";
 import styles from "./CampaignCard.module.css";
 
 export default async function CampaignCard({ campaign }: { campaign: Campaign }) {
-    const { id, name, createdAt, dungeonMaster, maxPlayers, outline } = campaign;
+    const { id, name, created_at, max_players, outline, dungeon_master_name } = campaign;
 
     return (
         <div className={ styles.card }>
             <h2>{ name }</h2>
             <img className={ styles.cardImage } src="https://placehold.co/400" alt="Placeholder"/>
             <p><strong>ID:</strong> { id }</p>
-            <p><strong>Created At:</strong> { new Date(createdAt).toLocaleDateString() }</p>
-            <p><strong>Dungeon Master:</strong> { dungeonMaster.displayName }</p>
-            <p><strong>Max Players:</strong> { maxPlayers }</p>
+            <p><strong>Created At:</strong> { new Date(created_at).toLocaleDateString() }</p>
+            <p><strong>Dungeon Master:</strong> { dungeon_master_name }</p>
+            <p><strong>Max Players:</strong> { max_players }</p>
             <p><strong>Outline:</strong> { outline }</p>
         </div>
     );

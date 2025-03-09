434679be9c46b8c888a311cc14c9fa5392321015
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-09 18:10:37 +0000

    Add Edit character form to character actions dropdown - Add a new dialog component - Put the form in the dialog

--- src/components/characters/CharacterActionsDropdown.tsx
+++ src/components/characters/CharacterActionsDropdown.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import EditCharacterForm from "@/components/characters/EditCharacterForm";
 import {
     AlertDialog,
     AlertDialogAction,
@@ -12,6 +13,7 @@ import {
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -19,17 +21,22 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { deleteCharacter } from "@/lib/actions/characters";
+import { Character } from "@/types/Character";
+import { Race } from "@/types/Race";
 import { Ellipsis, PencilIcon, Trash2Icon } from "lucide-react";
 import { redirect } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
 
-export default function CharacterActionsDropdown({ characterId }: { characterId: number }) {
+export default function CharacterActionsDropdown({ character, races }: { character: Character, races: Race[] }) {
+    const [ isOpen, setOpen ] = useState(false);
+
     async function handleEdit() {
         console.log("Editing character");
     }
 
     async function handleDelete() {
-        const response = await deleteCharacter(characterId);
+        const response = await deleteCharacter(character.id);
 
         if (response.ok) {
             toast(response.message);
@@ -39,36 +46,48 @@ export default function CharacterActionsDropdown({ characterId }: { characterId:
         }
     }
 
-    return <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <Button variant="ghost"><Ellipsis size={ 24 }/></Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuItem onSelect={ handleEdit }>
-                <PencilIcon/>
-                <span>Edit</span>
-            </DropdownMenuItem>
-            <AlertDialog>
-                <AlertDialogTrigger asChild>
-                    <DropdownMenuItem className="text-red-300" onSelect={ e => e.preventDefault() }>
-                        <Trash2Icon/>
-                        <span>Delete</span>
-                    </DropdownMenuItem>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            Do you really want to delete this character?
-                            <b className="block text-destructive mt-2">This cannot be undone.</b>
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction onClick={ handleDelete }>Delete my character</AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
-        </DropdownMenuContent>
-    </DropdownMenu>;
+    return <>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost"><Ellipsis size={ 24 }/></Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem onSelect={ () => setOpen(true) }>
+                    <PencilIcon/>
+                    <span>Edit</span>
+                </DropdownMenuItem>
+                <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                        <DropdownMenuItem className="text-red-300" onSelect={ e => e.preventDefault() }>
+                            <Trash2Icon/>
+                            <span>Delete</span>
+                        </DropdownMenuItem>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Do you really want to delete this character?
+                                <b className="block text-destructive mt-2">This cannot be undone.</b>
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={ handleDelete }>Delete my character</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            </DropdownMenuContent>
+
+            <Dialog open={ isOpen } onOpenChange={ open => setOpen(open) }>
+                <DialogContent className="bg-theme">
+                    <DialogHeader>
+                        <DialogTitle>Edit Character</DialogTitle>
+                    </DialogHeader>
+                    <EditCharacterForm character={ character } races={ races } onSubmit={ () => setOpen(false) }/>
+                </DialogContent>
+            </Dialog>
+        </DropdownMenu>
+
+    </>;
 }

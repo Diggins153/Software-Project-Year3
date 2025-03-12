3dbc2ff445bc2843f848e2bb69c3205093378636
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-12 12:44:46 +0000

    Remove semicolon from characters

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateCampaignInviteCode(): string {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789;";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let result = "";
 
     for (let i = 0; i < 7; i++) {

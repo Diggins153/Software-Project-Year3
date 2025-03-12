a9b28dd1da14bead27b334779a2c21f81efe5101
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-12 19:40:14 +0000

    Create ensure session function to assert session and session.user are defined

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,7 @@
+import { auth } from "@/lib/auth";
 import { clsx, type ClassValue } from "clsx";
+import { Session } from "next-auth";
+import { redirect } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,4 +17,26 @@ export function generateCampaignInviteCode(): string {
     }
 
     return result;
+}
+
+/**
+ * Ensures session and user is set. If session is unset, redirects the user to a specified page.
+ * @param redirectTo - Where the user is redirected if session is undefined.
+ * @return Session object
+ */
+export async function ensureSession(redirectTo: string = "/"): Promise<Required<Session>> {
+    let session = await auth();
+
+    if (!session || !session.user) {
+        if (typeof window === "undefined") {
+            redirect(redirectTo);
+        } else {
+            const { useRouter } = await import("next/navigation");
+            const router = useRouter();
+            router.push(redirectTo);
+        }
+        throw Error("Could not fetch session.");
+    }
+
+    return <Required<Session>>session;
 }

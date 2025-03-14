e584f90924c10752abe043470c31f39d3dbb34f1
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-14 21:22:18 +0000

    Add report content type dialog name

--- src/types/Report.ts
+++ src/types/Report.ts
@@ -20,6 +20,21 @@ export enum ContentType {
     MESSAGE = "message",
 }
 
+export const getContentTypeDialogName = (type: ContentType) => {
+    switch (type) {
+        case ContentType.USER:
+            return "Report a User";
+        case ContentType.CHARACTER:
+            return "Report a Character";
+        case ContentType.CAMPAIGN:
+            return "Report a Campaign";
+        case ContentType.SESSION:
+            return "Report a Session";
+        case ContentType.MESSAGE:
+            return "Report a Message";
+    }
+};
+
 export const campaignReasons = {
     "name": "Offensive Name",
     "outline": "Offensive Outline",

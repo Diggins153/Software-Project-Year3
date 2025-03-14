5c74d4adf1ac5e0b06dee20b4a92f59c7d2f42ec
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-14 14:25:48 +0000

    Add users table with actions

--- src/app/@appRoutes/campaigns/[campaignId]/manage/page.tsx
+++ src/app/@appRoutes/campaigns/[campaignId]/manage/page.tsx
@@ -1,7 +1,12 @@
 import InviteDialog from "@/components/campaigns/InviteDialog";
+import ManageCampaignCharacters from "@/components/campaigns/ManageCampaignCharacters";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import query from "@/lib/database";
 import { ensureSession, generateCampaignInviteCode } from "@/lib/utils";
 import { Campaign } from "@/types/Campaign";
+import { CharacterStatus } from "@/types/CampaignCharacters";
+import { Character } from "@/types/Character";
 import { Metadata } from "next";
 import { notFound, redirect } from "next/navigation";
 
@@ -10,12 +15,15 @@ export const metadata: Metadata = {
 };
 
 export default async function ManageCampaignPage({ params }: { params: Promise<{ campaignId: number }> }) {
-    let session = await ensureSession();
-
+    const session = await ensureSession();
     const { campaignId } = await params;
     const campaign = (await query<Campaign[]>("SELECT * FROM campaign WHERE id = ?", campaignId))[0] ?? null;
     if (!campaign) notFound();
     if (campaign.dungeon_master_id.toString() !== session.user.id) redirect(`/campaigns/view?campaignId=${ campaignId }`);
+    const characters = await query<(Character & { owner_name: string, status: CharacterStatus })[]>(
+        "SELECT c.*, u.display_name AS owner_name, campaign_characters.status FROM campaign_characters JOIN `character` c ON character_id = c.id JOIN user u ON c.owner_id = u.id WHERE campaign_id = ?",
+        campaignId,
+    );
 
     if (!campaign.invite) {
         const code = generateCampaignInviteCode();
@@ -23,10 +31,57 @@ export default async function ManageCampaignPage({ params }: { params: Promise<{
         campaign.invite = code;
     }
 
-    return <main>
-        <h1>Managing Campaign with ID { campaignId }</h1>
-        <InviteDialog inviteCode={ campaign.invite } campaignId={ campaignId }/>
-        <p>Table of users here</p>
-        <p>Campaign Actions Here</p>
+    function capitalize(string: string) {
+        return string[0].toUpperCase() + string.substring(1);
+    }
+
+    return <main className="w-full md:w-1/2 mx-auto space-y-8">
+        <div className="flex items-center justify-between">
+            <h1 className="text-2xl">Managing Campaign with ID { campaignId }</h1>
+            <InviteDialog inviteCode={ campaign.invite } campaignId={ campaignId }/>
+        </div>
+        <div className="space-y-2">
+            <h2 className="text-xl">Characters</h2>
+            { characters.length > 0 ?
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead></TableHead>
+                            <TableHead>Character</TableHead>
+                            <TableHead>Owner</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead className="text-right"></TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        { characters.map(character =>
+                            <TableRow key={ character.id }>
+                                <TableCell>
+                                    <Avatar>
+                                        <AvatarImage src={ character.image }/>
+                                        <AvatarFallback>{ character.name[0] }</AvatarFallback>
+                                    </Avatar>
+                                </TableCell>
+                                <TableCell>{ character.name }</TableCell>
+                                <TableCell>{ character.owner_name }</TableCell>
+                                <TableCell>{ capitalize(character.status) }</TableCell>
+                                <TableCell className="text-right">
+                                    <ManageCampaignCharacters
+                                        status={ character.status }
+                                        character={ character as Character }
+                                        campaignId={ campaignId }
+                                    />
+                                </TableCell>
+                            </TableRow>,
+                        ) }
+                    </TableBody>
+                </Table>
+                :
+                <p>You don't have any characters in your campaign. Time to invite some!</p>
+            }
+        </div>
+        <div>
+            <h2 className="text-xl">Campaign Actions Here</h2>
+        </div>
     </main>;
 };

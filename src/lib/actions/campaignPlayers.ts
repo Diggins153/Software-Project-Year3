69775680a60470398b956b2be396460bbd7a9946
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-14 13:39:51 +0000

    Create and use ensure user is DM

--- src/lib/actions/campaignPlayers.ts
+++ src/lib/actions/campaignPlayers.ts
@@ -2,6 +2,7 @@
 
 import query from "@/lib/database";
 import { ensureSession } from "@/lib/utils";
+import { Campaign } from "@/types/Campaign";
 import { Character } from "@/types/Character";
 
 type CampaignPlayerResponse = {
@@ -16,17 +17,25 @@ async function ensureCharacter(characterId: number): Promise<Character | null> {
 }
 
 async function ensureCharacterInCampaign(characterId: number, campaignId: number): Promise<boolean> {
-    const result = (await query<{ is_present: boolean }[]>(
+    const result = await query<{ is_present: boolean }[]>(
         "SELECT TRUE AS is_present FROM campaign_characters WHERE campaign_id = ? AND character_id = ?",
         campaignId, characterId,
-    ));
+    );
     if (result.length != 1) return false;
     return result[0].is_present;
+}
 
+async function ensureUserIsDungeonMaster(campaignId: number) {
+    const session = await ensureSession();
+    const campaigns = await query<Campaign[]>("SELECT dungeon_master_id FROM campaign WHERE id = ?", campaignId);
+
+    if (campaigns.length != 0) return false;
+    return campaigns[0].dungeon_master_id.toString() === session.user.id;
 }
 
 export async function kickPlayer(campaignId: number, characterId: number): Promise<CampaignPlayerResponse> {
-    await ensureSession();
+    const isDM = await ensureUserIsDungeonMaster(campaignId);
+    if (!isDM) return { ok: false, message: "You can only manage your own campaigns" };
     const character = await ensureCharacter(characterId);
     if (character === null) return { ok: false, message: "Could not find the specified character." };
     if (!await ensureCharacterInCampaign(characterId, campaignId))

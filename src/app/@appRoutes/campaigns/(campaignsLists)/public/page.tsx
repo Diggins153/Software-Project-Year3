8d6b0e9d737242cf9f4f9b74edae1cb85ad4ea46
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-15 16:09:13 +0000

    Add public campaigns list

---
+++ src/app/@appRoutes/campaigns/(campaignsLists)/public/page.tsx
@@ -0,0 +1,30 @@
+import CampaignCard from "@/components/CampaignCard";
+import query from "@/lib/database";
+import { ensureSession } from "@/lib/utils";
+import { Campaign } from "@/types/Campaign";
+import Link from "next/link";
+
+export default async function DungeonMasterCampaignsPage() {
+    const { user } = await ensureSession();
+    const campaigns = await query<Campaign[]>(`
+        SELECT c.*, u.display_name AS dungeon_master_name
+        FROM campaign c
+                 JOIN \`user\` u ON u.id = c.dungeon_master_id
+        WHERE c.public = TRUE
+    `, user.id);
+
+    return <main>
+        { campaigns.length > 0
+            ? <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                { campaigns.map((campaign) => (
+                    <Link key={ campaign.id } href={ `/campaigns/${ campaign.id }` }>
+                        <CampaignCard campaign={ campaign }/>
+                    </Link>
+                )) }
+            </div>
+            : <div className="flex justify-center mt-10">
+                There aren't any public campaigns
+            </div>
+        }
+    </main>;
+}

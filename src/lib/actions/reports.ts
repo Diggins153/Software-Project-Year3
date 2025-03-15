c2dabc08a29f9ceb155c8ef1820f9463db797d50
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-15 17:33:36 +0000

    Create ban user action

--- src/lib/actions/reports.ts
+++ src/lib/actions/reports.ts
@@ -5,8 +5,11 @@ import { deleteSession } from "@/lib/actions/sessions";
 import query from "@/lib/database";
 import { ReportContentFormSchema } from "@/lib/formSchemas";
 import { ensureSession } from "@/lib/utils";
+import { Campaign } from "@/types/Campaign";
+import { Character } from "@/types/Character";
 import { ContentType, Report } from "@/types/Report";
 import { Session } from "@/types/Session";
+import { User } from "@/types/User";
 import { z } from "zod";
 
 async function ensureReport(reportId: number) {
@@ -65,4 +68,65 @@ export async function removeContent(reportId: number): Promise<
     }
 
     return { ok: true, message: "Content removed" };
+}
+
+export async function banUser(reportId: number): Promise<
+    { ok: true } |
+    { ok: false, message: string }
+> {
+    await ensureSession();
+    const report = await ensureReport(reportId);
+    if (report === null) return { ok: false, message: "Could not find the specified report" };
+
+    let userId: number | null = null;
+
+    switch (report.content_type) {
+        case ContentType.USER:
+            userId = report.content_id;
+            break;
+        case ContentType.CHARACTER:
+            const character = (await query<Character[]>(`
+                SELECT owner_id
+                FROM \`character\`
+                WHERE id = ?
+            `, report.content_id))[0] || null;
+            if (character === null) return { ok: false, message: "Could not find owner of character" };
+            userId = character.owner_id;
+            break;
+        case ContentType.CAMPAIGN:
+            const campaign = (await query<Campaign[]>(`
+                SELECT dungeon_master_id
+                FROM campaign
+                WHERE id = ?
+            `, report.content_id))[0] || null;
+            if (campaign === null) return { ok: false, message: "Could not find DM of campaign" };
+            userId = campaign.dungeon_master_id;
+            break;
+        case ContentType.SESSION:
+            const user = (await query<User[]>(`
+                SELECT u.id
+                FROM session s
+                         JOIN campaign c ON s.campaign_id = c.id
+                         JOIN user u ON c.dungeon_master_id = u.id
+                WHERE s.id = ?
+            `, report.content_id))[0] || null;
+            if (user === null) {
+                return { ok: false, message: "Could not find author of session" };
+            }
+            userId = user.id;
+            break;
+        case ContentType.MESSAGE:
+            userId = null;
+            break;
+    }
+
+    if (userId == null) return { ok: false, message: "Unexpected error" };
+
+    await query(`
+        UPDATE user
+        SET banned = TRUE
+        WHERE id = ?
+    `, userId);
+
+    return { ok: true };
 }

f4b2767ec2dc70a1f739ce3a21320b02c47b2163
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-31 20:17:42 +0100

    Add top bar to settings page

--- src/app/@appRoutes/settings/page.tsx
+++ src/app/@appRoutes/settings/page.tsx
@@ -1,7 +1,9 @@
 import DeleteAccountDialog from "@/components/settings/DeleteAccountDialog";
 import UpdateUserForm from "@/components/settings/UpdateUserForm";
+import TopBar from "@/components/TopBar";
 import { ensureSession } from "@/lib/utils";
 import { User } from "@/types/User";
+
 /**
  * Settings page where users can update their details or delete their account.
  * @returns {Promise<JSX.Element>} - The settings page UI.
@@ -10,9 +12,7 @@ export default async function SettingsPage() {
     const { user } = await ensureSession();
 
     return <main>
-        <div className="bg-theme flex p-2 sticky top-0">
-            <h1 className="text-3xl font-bold">Settings</h1>
-        </div>
+        <TopBar title="Settings"/>
         <div className="p-1.5 w-full md:w-3/4 lg:w-1/2 xl:w-2/5 mx-auto space-y-4">
             <h2 className="text-xl font-bold">Update Details</h2>
             <UpdateUserForm user={ { ...user, id: Number(user.id) } as User }/>

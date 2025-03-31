5d3bbbc3309b8518309c7eedc3ab0a8e6a25cd85
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-31 20:18:07 +0100

    Update top bar - Set backtext and backlink as optional

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,17 +5,19 @@ import type { ReactNode } from "react";
 
 interface TopBarParams {
     title: string;
-    backText: string;
-    backLink: string;
+    backText?: string;
+    backLink?: string;
     endContent?: ReactNode;
 }
 
-export default async function TopBar({ title, backLink, backText, endContent }: TopBarParams) {
+export default function TopBar({ title, backLink, backText, endContent }: TopBarParams) {
     return <div className="flex items-center justify-between p-2 sticky top-0 bg-theme z-50">
         <div className="flex items-center gap-2 md:gap-4">
-            <Link href={ backLink } className={ buttonVariants({ variant: "ghost" }) }>
-                <ArrowLeftIcon/><span className="hidden lg:inline">{ backText }</span>
-            </Link>
+            { backText && backLink &&
+                <Link href={ backLink } className={ buttonVariants({ variant: "ghost" }) }>
+                    <ArrowLeftIcon/><span className="hidden lg:inline">{ backText }</span>
+                </Link>
+            }
             <h1 className="text-2xl font-bold">{ title }</h1>
         </div>
         <div>{ endContent && (endContent) }</div>

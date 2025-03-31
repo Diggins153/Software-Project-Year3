74c6c74128c33c700ef3cd6d045b58dd91335921
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-31 22:58:38 +0100

    Remove unnecessary layout

--- src/app/@appRoutes/characters/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export default function CharactersLayout({ children }: { children: Readonly<ReactNode> }) {
-    return <div>
-        { children }
-    </div>;
-};

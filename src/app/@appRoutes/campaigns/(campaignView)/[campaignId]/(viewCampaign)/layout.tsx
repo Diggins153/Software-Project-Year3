8a89392e378db26ecbad026a0efb504aca72d11e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-22 16:51:33 +0100

    Query messages from DB when loading layout

--- src/app/@appRoutes/campaigns/(campaignView)/[campaignId]/(viewCampaign)/layout.tsx
+++ src/app/@appRoutes/campaigns/(campaignView)/[campaignId]/(viewCampaign)/layout.tsx
@@ -3,6 +3,7 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 import query from "@/lib/database";
 import { ensureSession } from "@/lib/utils";
 import { Character } from "@/types/Character";
+import { Message } from "@/types/Message";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -22,10 +23,19 @@ export default async function ViewCampaignLayout({ children, params }: ViewCampa
     const characters = await query<Character[]>(`
         SELECT c.*
         FROM campaign_characters cc
-        JOIN \`character\` c ON c.id = cc.character_id
+                 JOIN \`character\` c ON c.id = cc.character_id
         WHERE cc.campaign_id = ?
-        AND owner_id = ?
+          AND owner_id = ?
     `, campaignId, user.id);
+
+    const messages = await query<Message[]>(`
+        SELECT m.*,
+               c.name   AS author_name,
+               c.handle AS author_handle
+        FROM messages m
+                 JOIN \`character\` c ON c.id = m.author_id
+        WHERE campaign_id = ?
+    `, campaignId);
 
     return <div className="w-full">
         <ResizablePanelGroup direction="horizontal" autoSaveId={ campaignId.toString() }>
@@ -34,7 +44,11 @@ export default async function ViewCampaignLayout({ children, params }: ViewCampa
             </ResizablePanel>
             <ResizableHandle withHandle/>
             <ResizablePanel defaultSize={ 20 } maxSize={ 30 } minSize={ 15 } collapsible className="content ml-2">
-                <CampaignChat campaignId={ campaignId } characters={ characters }/>
+                <CampaignChat
+                    campaignId={ campaignId }
+                    characters={ characters }
+                    initialMessages={ messages }
+                />
             </ResizablePanel>
         </ResizablePanelGroup>
     </div>;

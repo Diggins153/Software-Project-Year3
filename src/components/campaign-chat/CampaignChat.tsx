b8327201dd736446d64777e59988632556927342
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-22 16:50:24 +0100

    Add initial messages prop to campaign chat

--- src/components/campaign-chat/CampaignChat.tsx
+++ src/components/campaign-chat/CampaignChat.tsx
@@ -12,61 +12,13 @@ import { ChangeEvent, useState } from "react";
 interface CampaignChatProps {
     campaignId: number;
     characters: Character[];
+    initialMessages?: Message[];
 }
 
-const messages: Message[] = [
-    {
-        id: 1,
-        campaign_id: 0,
-        author_id: 1,
-        author_name: "Author 1",
-        author_handle: "author1",
-        message: "Message 1",
-        sent_at: new Date(2025, 3, 9, 17, 45),
-    },
-    {
-        id: 2,
-        campaign_id: 0,
-        author_id: 2,
-        author_name: "Author 2",
-        author_handle: "author2",
-        message: "Message 2",
-        sent_at: new Date(2025, 3, 9, 17, 46),
-    },
-    {
-        id: 3,
-        campaign_id: 0,
-        author_id: 1,
-        author_name: "Author 1",
-        author_handle: "author1",
-        message: "Some really really long text. This demonstrates wrapping of the text with the sent date.",
-        sent_at: new Date(2025, 3, 9, 17, 47),
-    },
-    {
-        id: 4,
-        campaign_id: 0,
-        author_id: 1,
-        author_name: "Author 1",
-        author_handle: "author1",
-        message: "Message 4\n" +
-            "This really long message demonstrates scrolling the chat view\n" +
-            "\n" +
-            "Totam velit aut doloribus. Totam ea nisi atque non nihil veniam quasi et. Repellendus voluptas sint autem voluptates. Ut tenetur reprehenderit quod amet magni laborum molestiae.\n" +
-            "\n" +
-            "Provident quisquam accusantium corporis impedit deserunt voluptatem. Aliquid sapiente provident provident at doloremque porro. Neque et voluptatem distinctio qui neque. Impedit consequatur iusto consectetur et aut. Qui sint nisi eius possimus repellat.\n" +
-            "\n" +
-            "Error placeat rerum tempore molestias harum accusamus sed quo. Voluptatibus possimus iure aliquid in itaque corporis cupiditate. Qui necessitatibus et eos odio possimus minima ducimus.\n" +
-            "\n" +
-            "Ullam rerum aut tempora itaque fugiat. Ut dolores et sint porro. Rerum cumque aspernatur et id. Amet repellat debitis vel corrupti. Mollitia voluptates nihil rem rerum. Porro aut quia aut facilis iusto dolorem quod molestiae.\n" +
-            "\n" +
-            "Unde autem ut dolor quisquam. Rerum et placeat voluptatum aut voluptas. Ut temporibus officia occaecati iste placeat. Quam aut ut nemo. Dolorum quibusdam animi voluptatem qui impedit fuga ea quo. Assumenda doloremque mollitia eos repellendus eos.\n",
-        sent_at: new Date(2025, 3, 9, 17, 47),
-    },
-];
-
-export default function CampaignChat({ campaignId, characters }: CampaignChatProps) {
+export default function CampaignChat({ campaignId, characters, initialMessages }: CampaignChatProps) {
     const [ character, setCharacter ] = useState<string>(characters?.[0]?.id.toString() ?? undefined);
     const [ message, setMessage ] = useState<string>("");
+    const [ messages, setMessages ] = useState<Message[]>(initialMessages ?? []);
 
     function handleMessageChange(e: ChangeEvent<HTMLTextAreaElement>) {
         setMessage(e.target.value);

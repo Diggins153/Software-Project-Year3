3193a2c8f8b98180610abac6fec8e64ee6126d2d
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-22 14:47:21 +0100

    Render newlines in messages correctly

--- src/components/campaign-chat/ChatMessage.tsx
+++ src/components/campaign-chat/ChatMessage.tsx
@@ -1,3 +1,4 @@
+import { artifika } from "@/lib/fonts";
 import { Message } from "@/types/Message";
 import Link from "next/link";
 
@@ -20,7 +21,8 @@ export default function ChatMessage({ message, showAuthor = true }: ChatMessageP
             </>
         }
         <div className="bg-yellow-200 text-black py-1 px-2 rounded-lg flex justify-between items-end flex-wrap">
-            { text }<span className="text-xs text-muted ms-auto">{ formatter.format(sent_at) }</span>
+            <pre className={ `text-wrap ${ artifika.className }` }>{ text }</pre>
+            <span className="text-xs text-muted ms-auto">{ formatter.format(sent_at) }</span>
         </div>
     </div>;
 }

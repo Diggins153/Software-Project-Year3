9a5def54ede141de79d5bf00dfbb70113b1a1cf8
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-23 18:10:34 +0100

    Add incoming message event name as const

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -13,4 +13,4 @@ export const pusherServer = new PusherServer({
 
 export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PSHR_KEY!, { cluster });
 
-export const INCOMING_MESSAGE_EVENT: string = "message::inc";
+export const INCOMING_MESSAGE_EVENT: string = "message__inc";

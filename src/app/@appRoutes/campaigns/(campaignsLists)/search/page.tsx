0f70353b6f4e99bc35adf1e4ca2bb8f283aecd97
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-04-25 15:36:59 +0100

    Create search page returning notFound

---
+++ src/app/@appRoutes/campaigns/(campaignsLists)/search/page.tsx
@@ -0,0 +1,9 @@
+import { notFound } from "next/navigation";
+
+export default function SearchPage() {
+    return notFound();
+
+    return <main>
+        <h3 className="text-3xl">Search page</h3>
+    </main>;
+}
